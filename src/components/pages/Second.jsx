29afe64bd2fc0bdfc7e13df87bfae0e0f0168ab3
JuriJuriM <jurimudry@gmail.com>
2024-06-26 16:11:11 +0300

    my first commit

---
+++ src/components/pages/Second.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Second.scss'
+
+const Second = () => {
+    return (
+        <div className='second'>Second</div>
+    )
+}
+
+export default Second
---
+++ src/components/pages/Third.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Third.scss'
+
+const Third = () => {
+    return (
+        <div className='third'>Third</div>
+    )
+}
+
+export default Third
